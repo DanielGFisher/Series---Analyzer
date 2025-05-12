2f7798790eebd5af0c054c3ed626c1f553423ba8
Daniel <Danielg16fisher@gmail.com>
2025-05-12 11:04:33 +0300

    Fixed 'System' Error

--- Series_Analyzer/Program.cs
+++ Series_Analyzer/Program.cs
@@ -1,3 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
-System.Console.WriteLine("This is different!");
+Console.WriteLine("This is different!");
