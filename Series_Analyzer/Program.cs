c136caf54ffecbb5616811773591888c468628e7
Daniel <Danielg16fisher@gmail.com>
2025-05-12 15:12:33 +0300

    Added return to NewSeries()

--- Series_Analyzer/Program.cs
+++ Series_Analyzer/Program.cs
@@ -1,10 +1,21 @@
-﻿bool flag = true;
-void Main()
+﻿using System.Collections.Generic;
+using System.ComponentModel.Design;
+
+bool flagMain = true;
+bool flagMenu = true;
+
+List<int> Main(string[] args)
 {
-
+    args.ToList<string>();
+    while (flagMain == true)
+    {
+        Console.WriteLine("--- Welcome to our Analyzer! ---");
+        Verify(args);
+        NewSeries(args);
+    }
 }
-
-void Menu()
+Menu(args);
+void Menu(List<string> list)
 {
     Console.WriteLine("--- Menu ---");
     Console.WriteLine("1) Insert New Series:\n2) Display Current Order\n3) Display Reversed Order:\n4) Display Sorted Order:\n5) Display Largest Series Value:\n6) Display Lowest Series Value:\n7) Display Series Average:\n8) Display Series Sum:\n9) Exit Program:");
@@ -14,31 +25,31 @@ void Menu()
     switch (choice)
     {
         case 1:
-            NewSeries();
+            NewSeries(list);
             break;
 
         case 2:
-            CurrentOrder();
+            CurrentOrder(list);
             break;
 
         case 3:
-            ReverseOrder();
+            ReverseOrder(list);
             break;
 
         case 4:
-            SortedOrder();
+            SortedOrder(list);
             break;
 
         case 5:
-            SeriesMax();
+            SeriesMax(list);
             break;
 
         case 6:
-            SeriesMin();
+            SeriesMin(list);
             break;
 
         case 7:
-            SeriesAverage();
+            SeriesAverage(list);
             break;
 
         case 8:
@@ -52,42 +63,86 @@ void Menu()
 
 }
 
-void Verify()
+List<string> Verify(List<string> list)
+{
+    int count = 0;
+
+    for (int i = 0; i < args.Length; i++)
+    {
+        if (int.Parse(args[i]) > 0) count += 1;
+    }
+    if (count >= 3) 
+    {
+        Console.WriteLine("Verified Successfully!");
+        flagMain = false;
+    }
+    else Console.WriteLine("Inputs are invalid!");
+}
+
+List<string> NewSeries(List<string> list)
+{
+    foreach (var item in list)
+    {
+        list.Remove(item);
+    }
+
+    bool flag = true;
+    while (flag == true)
+    {
+        Console.WriteLine("--- New Series ---");
+        Console.WriteLine("1) Add New Numbers:\n2) Exit:");
+        Console.WriteLine("Insert a choice by number:");
+        int choice = int.Parse(Console.ReadLine());
+
+        switch (choice)
+        {
+            case 1:
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.WriteLine("Please insert your number:");
+                    string num = Console.ReadLine();
+                }
+                flag = false;
+                break;
+
+            case 2:
+                flag = false;
+                break;
+
+            default:
+                Console.WriteLine("Invalid Input!");
+                break;
+        }
+    }
+    return list;
+}
+
+void CurrentOrder(string[] list)
 {
 
 }
 
-void NewSeries()
+void ReverseOrder(string[] list)
 {
 
 }
 
-void CurrentOrder()
+void SortedOrder(string[] list)
 {
 
 }
 
-void ReverseOrder()
+void SeriesMax(string[] list)
 {
 
 }
 
-void SortedOrder()
+void SeriesMin(string[] list)
 {
 
 }
 
-void SeriesMax()
-{
-
-}
-
-void SeriesMin()
-{
-
-}
-
-void SeriesAverage()
+void SeriesAverage(string[] list)
 {
 
 }
